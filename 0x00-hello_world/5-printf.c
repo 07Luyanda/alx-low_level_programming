ea0b1384adcf3523adcf3bab88048559954a6524
07Luyanda <luyandahlatshwayo07@gmail.com>
2023-06-15 19:08:32 -0700

    0-preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - A program that print a line using printf function
- * Return: o (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-		return (O);
-}
+ * *Description: main - Prints out a string out a string to stdout.
+ * *Return: 0 if success.
+ *\
+ int main(void)
+ {
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return(O)
+ }
